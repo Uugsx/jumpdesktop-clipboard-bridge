94fecb48654b0a38173b428fab28054479b379a5
uugsx <nikita.galkin@dpd.ru>
2026-07-10 11:38:26 +0300

    Update Swift bridge to be App-Aware (fixes local Mac pasting)

--- ClipboardBridge.swift
+++ ClipboardBridge.swift
@@ -1,6 +1,8 @@
 import Cocoa
 import Foundation
 
+let jumpBundleId = "com.p5sys.jump.mac.viewer"
+
 func getPasteboardChangeCount() -> Int {
     return NSPasteboard.general.changeCount
 }
@@ -11,44 +13,66 @@ func processClipboard() {
     
     let hasImage = types.contains(.tiff) || types.contains(.png)
     
-    var isProcessed = false
+    let frontmostAppId = NSWorkspace.shared.frontmostApplication?.bundleIdentifier
+    let isJumpActive = (frontmostAppId == jumpBundleId)
+    
+    var hasBase64Payload = false
     if types.contains(.string), let currentText = pasteboard.string(forType: .string) {
         if currentText.hasPrefix("CLIPBOARD_IMAGE_BASE64:") {
-            isProcessed = true
+            hasBase64Payload = true
         }
     }
     
-    if hasImage && !isProcessed {
-        guard let tiffData = pasteboard.data(forType: .tiff) else { return }
-        
-        var base64String = ""
-        if let image = NSImage(data: tiffData),
-           let tiffRepresentation = image.tiffRepresentation,
-           let bitmapImage = NSBitmapImageRep(data: tiffRepresentation),
-           let pngData = bitmapImage.representation(using: .png, properties: [:]) {
-            base64String = pngData.base64EncodedString()
-        } else {
-            base64String = tiffData.base64EncodedString()
+    if hasImage {
+        if isJumpActive && !hasBase64Payload {
+            print("Jump Desktop active. Writing Base64 representation...")
+            guard let tiffData = pasteboard.data(forType: .tiff) else { return }
+            
+            var base64String = ""
+            if let image = NSImage(data: tiffData),
+               let tiffRepresentation = image.tiffRepresentation,
+               let bitmapImage = NSBitmapImageRep(data: tiffRepresentation),
+               let pngData = bitmapImage.representation(using: .png, properties: [:]) {
+                base64String = pngData.base64EncodedString()
+            } else {
+                base64String = tiffData.base64EncodedString()
+            }
+            
+            let textPayload = "CLIPBOARD_IMAGE_BASE64:" + base64String
+            
+            let item = NSPasteboardItem()
+            item.setData(tiffData, forType: .tiff)
+            item.setString(textPayload, forType: .string)
+            
+            pasteboard.clearContents()
+            pasteboard.writeObjects([item])
+            
+        } else if !isJumpActive && hasBase64Payload {
+            print("Jump Desktop inactive. Removing Base64 representation...")
+            guard let tiffData = pasteboard.data(forType: .tiff) else { return }
+            
+            let item = NSPasteboardItem()
+            item.setData(tiffData, forType: .tiff)
+            
+            pasteboard.clearContents()
+            pasteboard.writeObjects([item])
         }
-        
-        let textPayload = "CLIPBOARD_IMAGE_BASE64:" + base64String
-        
-        let item = NSPasteboardItem()
-        item.setData(tiffData, forType: .tiff)
-        item.setString(textPayload, forType: .string)
-        
-        pasteboard.clearContents()
-        pasteboard.writeObjects([item])
     }
 }
 
 func main() {
+    print("Starting Swift Clipboard App-Aware Bridge Daemon...")
     var lastChangeCount = getPasteboardChangeCount()
+    var lastFrontmostAppId: String? = nil
+    
     while true {
         let currentChangeCount = getPasteboardChangeCount()
-        if currentChangeCount != lastChangeCount {
+        let currentFrontmostAppId = NSWorkspace.shared.frontmostApplication?.bundleIdentifier
+        
+        if currentChangeCount != lastChangeCount || currentFrontmostAppId != lastFrontmostAppId {
             processClipboard()
             lastChangeCount = getPasteboardChangeCount()
+            lastFrontmostAppId = currentFrontmostAppId
         }
         Thread.sleep(forTimeInterval: 0.1)
     }
