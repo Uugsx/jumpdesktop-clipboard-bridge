1e16e57cd140a7f3dc25aa02d421b2c9c2482710
uugsx <nikita.galkin@dpd.ru>
2026-07-13 10:33:37 +0300

    Fix Swift bridge background execution using Cocoa RunLoop (resolves background active app check)

--- ClipboardBridge.swift
+++ ClipboardBridge.swift
@@ -3,79 +3,100 @@ import Foundation
 
 let jumpBundleId = "com.p5sys.jump.mac.viewer"
 
-func getPasteboardChangeCount() -> Int {
-    return NSPasteboard.general.changeCount
-}
-
-func processClipboard() {
-    let pasteboard = NSPasteboard.general
-    guard let types = pasteboard.types else { return }
+class ClipboardAgent: NSObject {
+    var lastChangeCount = NSPasteboard.general.changeCount
     
-    let hasImage = types.contains(.tiff) || types.contains(.png)
-    
-    let frontmostAppId = NSWorkspace.shared.frontmostApplication?.bundleIdentifier
-    let isJumpActive = (frontmostAppId == jumpBundleId)
-    
-    var hasBase64Payload = false
-    if types.contains(.string), let currentText = pasteboard.string(forType: .string) {
-        if currentText.hasPrefix("CLIPBOARD_IMAGE_BASE64:") {
-            hasBase64Payload = true
+    override init() {
+        super.init()
+        
+        // Register active application change observer
+        NSWorkspace.shared.notificationCenter.addObserver(
+            self,
+            selector: #selector(activeAppChanged),
+            name: NSWorkspace.didActivateApplicationNotification,
+            object: nil
+        )
+        
+        // Timer for clipboard checking (every 0.2 seconds)
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { [weak self] _ in
+            self?.checkClipboard()
         }
     }
     
-    if hasImage {
-        if isJumpActive && !hasBase64Payload {
-            print("Jump Desktop active. Writing Base64 representation...")
-            guard let tiffData = pasteboard.data(forType: .tiff) else { return }
-            
-            var base64String = ""
-            if let image = NSImage(data: tiffData),
-               let tiffRepresentation = image.tiffRepresentation,
-               let bitmapImage = NSBitmapImageRep(data: tiffRepresentation),
-               let pngData = bitmapImage.representation(using: .png, properties: [:]) {
-                base64String = pngData.base64EncodedString()
-            } else {
-                base64String = tiffData.base64EncodedString()
-            }
-            
-            let textPayload = "CLIPBOARD_IMAGE_BASE64:" + base64String
-            
-            let item = NSPasteboardItem()
-            item.setData(tiffData, forType: .tiff)
-            item.setString(textPayload, forType: .string)
-            
-            pasteboard.clearContents()
-            pasteboard.writeObjects([item])
-            
-        } else if !isJumpActive && hasBase64Payload {
-            print("Jump Desktop inactive. Removing Base64 representation...")
-            guard let tiffData = pasteboard.data(forType: .tiff) else { return }
-            
-            let item = NSPasteboardItem()
-            item.setData(tiffData, forType: .tiff)
-            
-            pasteboard.clearContents()
-            pasteboard.writeObjects([item])
+    @objc func activeAppChanged(_ notification: Notification) {
+        processClipboard()
+    }
+    
+    func checkClipboard() {
+        let currentChangeCount = NSPasteboard.general.changeCount
+        if currentChangeCount != lastChangeCount {
+            lastChangeCount = currentChangeCount
+            processClipboard()
         }
+    }
+    
+    func processClipboard() {
+        let pasteboard = NSPasteboard.general
+        guard let types = pasteboard.types else { return }
+        
+        let hasImage = types.contains(.tiff) || types.contains(.png)
+        let frontmostAppId = NSWorkspace.shared.frontmostApplication?.bundleIdentifier
+        let isJumpActive = (frontmostAppId == jumpBundleId)
+        
+        var hasBase64Payload = false
+        if types.contains(.string), let currentText = pasteboard.string(forType: .string) {
+            if currentText.hasPrefix("CLIPBOARD_IMAGE_BASE64:") {
+                hasBase64Payload = true
+            }
+        }
+        
+        if hasImage {
+            if isJumpActive && !hasBase64Payload {
+                guard let tiffData = pasteboard.data(forType: .tiff) else { return }
+                
+                var base64String = ""
+                if let image = NSImage(data: tiffData),
+                   let tiffRepresentation = image.tiffRepresentation,
+                   let bitmapImage = NSBitmapImageRep(data: tiffRepresentation),
+                   let pngData = bitmapImage.representation(using: .png, properties: [:]) {
+                    base64String = pngData.base64EncodedString()
+                } else {
+                    base64String = tiffData.base64EncodedString()
+                }
+                
+                let textPayload = "CLIPBOARD_IMAGE_BASE64:" + base64String
+                
+                let item = NSPasteboardItem()
+                item.setData(tiffData, forType: .tiff)
+                item.setString(textPayload, forType: .string)
+                
+                self.lastChangeCount = currentChangeCount() + 1
+                pasteboard.clearContents()
+                pasteboard.writeObjects([item])
+                
+            } else if !isJumpActive && hasBase64Payload {
+                guard let tiffData = pasteboard.data(forType: .tiff) else { return }
+                
+                let item = NSPasteboardItem()
+                item.setData(tiffData, forType: .tiff)
+                
+                self.lastChangeCount = currentChangeCount() + 1
+                pasteboard.clearContents()
+                pasteboard.writeObjects([item])
+            }
+        }
+    }
+    
+    private func currentChangeCount() -> Int {
+        return NSPasteboard.general.changeCount
     }
 }
 
 func main() {
-    print("Starting Swift Clipboard App-Aware Bridge Daemon...")
-    var lastChangeCount = getPasteboardChangeCount()
-    var lastFrontmostAppId: String? = nil
+    let _ = ClipboardAgent()
     
-    while true {
-        let currentChangeCount = getPasteboardChangeCount()
-        let currentFrontmostAppId = NSWorkspace.shared.frontmostApplication?.bundleIdentifier
-        
-        if currentChangeCount != lastChangeCount || currentFrontmostAppId != lastFrontmostAppId {
-            processClipboard()
-            lastChangeCount = getPasteboardChangeCount()
-            lastFrontmostAppId = currentFrontmostAppId
-        }
-        Thread.sleep(forTimeInterval: 0.1)
-    }
+    // Start CFRunLoop to listen for notifications and timers
+    CFRunLoopRun()
 }
 
 main()
